5e8ee1db640dc4a3502526b909a4019464abcdaa
harry343 <isitharry1@gmail.com>
2017-12-16 11:59:21 +0530

    migrate profile table

--- database/migrations/2017_12_15_060928_create_profiles_table.php
+++ database/migrations/2017_12_15_060928_create_profiles_table.php
@@ -15,7 +15,15 @@ class CreateProfilesTable extends Migration
     {
         Schema::create('profiles', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('firstname', 100);
+            $table->string('lastname', 100);
+            $table->date('dateofbirth');
+            $table->string('country');
+            $table->boolean('profile_image');
+            $table->text('description');
             $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
